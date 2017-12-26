89cac277927389afe45665fab92b9aa94bd5fc8f
SACHIN5SOS <sachinb443@gmail.com>
2017-12-26 05:57:55 +0530

    take input from user through process.argv

--- notes-nodes/app.js
+++ notes-nodes/app.js
@@ -1,19 +1,32 @@
 console.log('Starting app.');
 
-const fs = require('fs');
+// const fs = require('fs');
 
-const os= require('os');
+// const os= require('os');
+
+// const _ = require('lodash');   //requiring module of lodash package
+
+// console.log(_.isString(true));   //lodash module
+// console.log(_.isString('Sachin'));
 const notes= require('./notes.js');
-const _ = require('lodash');   //requiring module of lodash package
+var command = process.argv[2];  //for taking user input
+console.log('Command: ', command);
 
-console.log(_.isString(true));   //lodash module
-console.log(_.isString('Sachin'));
+if(command==='add'){
+    console.log('Adding the list');
+}
+else if(command==='list')
+{
+    console.log('Listing the node');
+}
+else
+console.log('command not recognised');
+// // const newNote = notes.addNote();
+// // console.log(newNote);
+// // var user= os.userInfo();
+// // //console.log(user);
+// // fs.appendFile('greeting.txt',`Hello ${user.username}! You are ${notes.age}.`);
 
-// const newNote = notes.addNote();
-// console.log(newNote);
-// var user= os.userInfo();
-// //console.log(user);
-// fs.appendFile('greeting.txt',`Hello ${user.username}! You are ${notes.age}.`);
+// const addnumber = notes.add(12,5);
+// console.log(addnumber);
 
-const addnumber = notes.add(12,5);
-console.log(addnumber);
