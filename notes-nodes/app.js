c26b3ada7d0d65acfb1e4c9313b75e68713c8af4
SACHIN5SOS <sachinb443@gmail.com>
2017-12-26 07:09:25 +0530

    Introducing Yargs
    Yargs is a npm package which is used to take input and simplify the properties of those input.  It is a alternative for process.argv.

--- notes-nodes/app.js
+++ notes-nodes/app.js
@@ -8,25 +8,46 @@ console.log('Starting app.');
 
 // console.log(_.isString(true));   //lodash module
 // console.log(_.isString('Sachin'));
+//Lodash AND OS FUNCTIONALITIES
+/* 
+// const newNote = notes.addNote();
+// console.log(newNote);
+// var user= os.userInfo();
+// //console.log(user);
+// fs.appendFile('greeting.txt',`Hello ${user.username}! You are ${notes.age}.`);
+
+const addnumber = notes.add(12,5);
+console.log(addnumber);
+
+ */
 const notes= require('./notes.js');
-var command = process.argv[2];  //for taking user input
-console.log('Command: ', command);
+/* 
+//for taking output with provcess.argv
+
+console.log('Command ', command);
+console.log('Processse',process.argv);  //procee.argv 
+*/
+
+const yargs = require('yargs');  //It is good for key value pair.. Like   for   node app.js add --tittle "Success"  
+
+const yarg_input = yargs.argv; //for yarg input/* 
+var command = yarg_input._[0];    //for taking user input alternative is process.argv[2];
+
+console.log('Yargs": ',yarg_input);    //printg yarg output like Yarrgs = ['_': tittle : 'Sucess']
 
 if(command==='add'){
-    console.log('Adding the list');
+    notes.addNote(yarg_input.title,yarg_input.body);
 }
 else if(command==='list')
 {
-    console.log('Listing the node');
+    notes.getAll();
+}
+else if(command=='read'){
+    notes.getNote(yarg_input.title);
+}
+else if(command=='renove')
+{
+    notes.removeNote(yarg_input.title);
 }
 else
 console.log('command not recognised');
-// // const newNote = notes.addNote();
-// // console.log(newNote);
-// // var user= os.userInfo();
-// // //console.log(user);
-// // fs.appendFile('greeting.txt',`Hello ${user.username}! You are ${notes.age}.`);
-
-// const addnumber = notes.add(12,5);
-// console.log(addnumber);
-
--- notes-nodes/notes.js
+++ notes-nodes/notes.js
@@ -1,15 +1,40 @@
 
 
 console.log('Starting notes.js');
-// module.exports.age=19;
-
-// module.exports.addNote = () => {
-//     console.log('add notes');
-//     return ('New Nodes added');
-// }
-var a,b;
-module.exports.add = (a,b) =>
+var addNote=(title,body) =>
 {
-    console.log('Add a and b');
-    return (a+b);
+    console.log(title,body);
 }
+var getAll = () => {
+    console.log('Getting All lists');
+}
+var getNote = (title) => {
+    console.log('Reading ',title);
+}
+var removeNote = (title) =>
+{
+    console.log('Removing ', title);
+}
+module.exports = {
+    addNote,   //It means addNote:addNote
+    getAll,
+    getNote,
+    removeNote
+}
+
+
+
+
+
+// // module.exports.age=19;
+
+// // module.exports.addNote = () => {
+// //     console.log('add notes');
+// //     return ('New Nodes added');
+// // }
+// var a,b;
+// module.exports.add = (a,b) =>
+// {
+//     console.log('Add a and b');
+//     return (a+b);
+// }
