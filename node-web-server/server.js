a2ea28b2004e3cc9c567703c25b17e323112441a
SACHIN5SOS <sachinb443@gmail.com>
2018-01-03 14:26:53 +0530

    Server with express

--- node-web-server/server.js
+++ node-web-server/server.js
@@ -1,2 +1,22 @@
 const express= require('express');
+
 var app=express();
+app.get('/',(req,res)=>{
+    res.send({
+     name : 'Sachin',
+     likes: [
+         'Gaming',
+         'Music'
+     ]   
+    });
+});
+app.get('/about',(req,res)=>{
+ res.send('<h1>About Page</h1>');
+});
+app.get('/bad',(req,res)=>{
+    res.send({
+        errorMessage:'Not connceted'
+    })
+});
+
+app.listen(3000);
