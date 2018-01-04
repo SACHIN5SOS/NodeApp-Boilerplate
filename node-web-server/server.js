674be1b23e7f5bae0cd04178ec954541c7317b9d
SACHIN5SOS <sachinb443@gmail.com>
2018-01-04 22:35:10 +0530

    server.log added

--- node-web-server/server.js
+++ node-web-server/server.js
@@ -1,19 +1,38 @@
 const express= require('express');
 const hbs = require('hbs');   //handlebars
+const fs= require('fs');
+
 var app=express();
+
 app.set('viewengine','hbs');
+
 hbs.registerPartials(__dirname+'/views/partials');   //For using Partials in handlebars
-app.use(express.static(__dirname + '/public'));
+
+app.use(express.static(__dirname + '/public'));  //middleware static
+
+app.use((req,res,next)=>{
+    var now= new Date().toString();
+    var log= `${now}: ${req.method} ${req.url}`;
+    fs.appendFile('server.log',log+ '\n',(err)=>{
+        if(err){
+            control.log('Unable to proceed');
+        }
+    });
+    next();
+});
+
+
 hbs.registerHelper('screamIt',(text)=>{
     return text.toUpperCase()
-})
+});
+
 app.get('/',(req,res)=>{
     // res.send({
     //  name : 'Sachin',
     //  likes: [
     //      'Gaming',
     //      'Music'
-    //  ]   
+    //  ]    
     // });
     res.render('home.hbs',{
         pageTitle:'Home Page',
@@ -21,12 +40,14 @@ app.get('/',(req,res)=>{
         welcomeMessage: 'Welcome to my page'
     });
 });
+
 app.get('/about',(req,res)=>{
  res.render('about.hbs',{
      pageTitle: 'About Page',
      currentYear: new Date().getFullYear()
  });
 });
+
 app.get('/bad',(req,res)=>{
     res.send({
         errorMessage:'Not connceted'
@@ -36,3 +57,5 @@ app.get('/bad',(req,res)=>{
 app.listen(3000,()=>{
     console.log('Server is at port 3000');
 });
+
+
