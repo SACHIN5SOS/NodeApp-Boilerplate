92c011ba1c34a7a87a3677deae65bb60d5eb9874
SACHIN5SOS <sachinb443@gmail.com>
2018-01-06 23:35:54 +0530

    explaination with comment..

--- server.js
+++ server.js
@@ -1,17 +1,19 @@
-const express= require('express');
-const hbs = require('hbs');   //handlebars
-const fs= require('fs');
+const express= require('express');      //for access express module
+const hbs = require('hbs');   //for acceess over viewengine handlebars
+const fs= require('fs');  //for fs module
 
-const port=process.env.PORT || 3000;
+const port=process.env.PORT || 3000;  //declare port at 3000 as environment
 var app=express();
 
-app.set('viewengine','hbs');
+app.set('viewengine','hbs');  //specify viewengine to hbs handlebars.. So view folder will containe all /files.
 
-hbs.registerPartials(__dirname+'/views/partials');   //For using Partials in handlebars
+hbs.registerPartials(__dirname+'/views/partials');   //For using Partials in handlebars  //Register Partial for reusing a code
 
-app.use(express.static(__dirname + '/public'));  //middleware static
+app.use(express.static(__dirname + '/public'));  //middleware static for rendering data
 
-app.use((req,res,next)=>{
+
+//To recording all logs by users
+app.use((req,res,next)=>{  
     var now= new Date().toString();
     var log= `${now}: ${req.method} ${req.url}`;
     fs.appendFile('server.log',log+ '\n',(err)=>{
@@ -19,14 +21,18 @@ app.use((req,res,next)=>{
             control.log('Unable to proceed');
         }
     });
-    next();
+    next();   //without this it will not move on
 });
 
 
-hbs.registerHelper('screamIt',(text)=>{
+
+
+hbs.registerHelper('screamIt',(text)=>{   //helper for hbs
     return text.toUpperCase()
 });
 
+
+//rendering through hbs handlebars
 app.get('/',(req,res)=>{
     // res.send({
     //  name : 'Sachin',
@@ -35,12 +41,14 @@ app.get('/',(req,res)=>{
     //      'Music'
     //  ]    
     // });
-    res.render('home.hbs',{
+    res.render('home.hbs',{              
         pageTitle:'Home Page',
         currentYear: new Date().getFullYear(),
         welcomeMessage: 'Welcome to my page'
     });
 });
+
+
 
 app.get('/about',(req,res)=>{
 res.render('about.hbs',{
@@ -49,6 +57,7 @@ res.render('about.hbs',{
  });
 });
 
+//Rendering without handlebars
 app.get('/bad',(req,res)=>{
     res.send({
         errorMessage:'Not connceted'
@@ -60,6 +69,8 @@ app.get('/projects',(req,res)=>{
         pageTitle:'Project Page'
     });
 });
+
+
 app.listen(port,()=>{
     console.log(`Server is at port ${port}`);  
 });
